039f0915df9868c1bfa270a6af3c8dea2e54c63e
Eric Arbour <arboureric@gmail.com>
2020-04-10 14:45:48 -0400

    Set map to start on Boston and restrict pan bounds.

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,19 +1,45 @@
 import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
 
+const bostonCoordinates = { latitude: 42.361145, longitude: -71.057083 };
+const minLatitude = bostonCoordinates.latitude - 0.2; // Southern bound
+const maxLatitude = bostonCoordinates.latitude + 0.2; // Northern bound
+const minLongitude = bostonCoordinates.longitude - 0.2; // Western bound
+const maxLongitude = bostonCoordinates.longitude + 0.2; // Eastern bound
+
 export default function Map() {
-  const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
-  });
+  const [viewport, setViewport] = useState(() => ({
+    width: 800,
+    height: 800,
+    ...bostonCoordinates,
+    zoom: 12,
+    minZoom: 11,
+  }));
 
   return (
     <ReactMapGL
       {...viewport}
-      onViewportChange={(viewport) => setViewport(viewport)}
+      onViewportChange={(viewport) => {
+        const { latitude, longitude } = viewport;
+        const newLatitude =
+          latitude > maxLatitude
+            ? maxLatitude
+            : latitude < minLatitude
+            ? minLatitude
+            : latitude;
+        const newlongitude =
+          longitude > maxLongitude
+            ? maxLongitude
+            : longitude < minLongitude
+            ? minLongitude
+            : longitude;
+
+        setViewport({
+          ...viewport,
+          latitude: newLatitude,
+          longitude: newlongitude,
+        });
+      }}
     />
   );
 }
